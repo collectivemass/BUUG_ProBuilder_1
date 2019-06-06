fbea7521e0f1f92b8ed066845e68f8ffa7ab9ac4
Roger Miller <roger@collectivemass.com>
2019-06-05 18:11:41 -0700

    Initial Upload

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BUUG.Game {
+
+	public class CameraFollow : MonoBehaviour {
+
+		// Public (Editor Exposed) Variables
+		public Transform whatToFollow;
+
+		// Private (Internal) Variables
+		private Transform localTransform;
+
+		private void Awake() {
+			localTransform = transform;
+		}
+
+		private void Update() {
+		
+			// Make the camera folow the target but only on the X and Y
+			Vector3 position = localTransform.position;
+			position.x = whatToFollow.position.x;
+			position.z = whatToFollow.position.z;
+			localTransform.position = position;
+		}
+	}
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace BUUG.Game {
+
+	public class Main : MonoBehaviour {
+
+		private const string AXIS_X = "Horizontal";
+		private const string AXIS_Z = "Vertical";
+		private const float AXIS_MULTI = 5;
+		private const float BALL_FAIL_HEIGHT = -1;
+
+		// Exposed Fields
+		public Transform tiltPlatform;
+		public Transform ball;
+
+		// Private Variables
+		private Vector3 eular = new Vector3();
+		private Vector3 ballStartPosition;
+
+		private void Awake() {
+			ballStartPosition = ball.position;
+		}
+
+		private void Update() {
+			Input_Process();
+			Ball_Process();
+		}
+
+		private void Input_Process() {
+
+			// Set the rotation
+			eular.z = Input.GetAxis(AXIS_X) * AXIS_MULTI;
+			eular.x = Input.GetAxis(AXIS_Z) * AXIS_MULTI;
+
+			// Bake teh rotation into the transform
+			tiltPlatform.eulerAngles = eular;
+		}
+
+		private void Ball_Process() {
+			if (ball.position.y < BALL_FAIL_HEIGHT) {
+				ball.position = ballStartPosition;
+				Rigidbody physics = ball.GetComponent<Rigidbody>();
+				physics.velocity = Vector3.zero;
+				physics.angularVelocity = Vector3.zero;
+			}
+		}
+	}
+}
